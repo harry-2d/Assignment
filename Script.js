5fde53bfd78b6e09b4acbd6b1c582263c845cb11
harry-2d <51414908+harry-2d@users.noreply.github.com>
2019-08-29 21:30:52 +0545

    r

--- Script.js
+++ Script.js
@@ -47,3 +47,16 @@ function refreshTime(){
     setInterval(currentTime, 1000);
 
 }
+
+function validations(){
+    var username=document.getElementById("e");
+    var password=document.getElementById("p");
+    
+    if(username=="" && password=="")
+    {
+        document.getElementById('error').innerHTML="username and password is invalid";
+        return false;
+    }
+    alert(hello);
+    return false;
+}
